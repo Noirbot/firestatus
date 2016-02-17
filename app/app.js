35ce93c83f4eac595fb38d57816e2a85113caf60
Perry Shuman <perry.shuman@bazaarvoice.com>
2016-02-17 09:39:53 -0600

    Skeleton app for Flynn testing.

---
+++ app/app.js
@@ -0,0 +1,37 @@
+/*
+ * Baseline Node service
+ *
+ * Fork this when creating new Curations microservices in Node.js
+ *
+ */
+
+var restify = require('restify');
+var logger = require('./logger');
+
+// Initialize Restify server
+var server = restify.createServer({
+  name: 'Firestatus'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.throttle({
+  rate: 1,
+  burst: 15,
+  ip: true
+}));
+
+// log all incoming requests with response code
+server.on('after', function(req, res, route, err) {
+  if (err) {
+    logger.info('REQ/RES: %s %s [%d]', req.method, req.url, err.status);
+  }
+  else {
+    logger.info('REQ/RES: %s %s [%d]', req.method, req.url, res.statusCode);
+  }
+});
+
+// Register all API modules
+require('./apiRoot')(server);
+
+module.exports = server;
